f03a7bbf19645d36cbe196cd46002d05aa0c31ba
sowmyavs1992 <60143575+sowmyavs1992@users.noreply.github.com>
2020-08-26 19:02:08 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,45 +1,67 @@
-function(input, output, session){
+shinyServer(
+  
+  function(input, output) {
+  #Sys.sleep(2)
+  #waiter_hide()
   
   ## Home page -----------------------------------------------------------------
   # output$date_updated <- renderText({glue("Last updated: ", "{Sys.Date()}")}) # TO DO: update this with data load
   
   # TO DO: replace this with function that builds epi curve for all data
-  output$epi_curve_total <- renderPlot({
-    ggplot(data=data.frame(x=1:10, y=1:10), aes(x, y)) + geom_point()
+  
+    #Created a reactive dataset. Sometimes render functions work better with dynamic output
+    
+    data_random <- reactive({ 
+    data.frame(x=sample(1:10),y=sample(1:10))
+    
   })
   
-  # TO DO: read in/format statewide values to create boxes with appropriate colors
-  output$state_cases <- renderUI({
-    box(
-      p("# positive cases"), 
-      p("14-day"),
-      background = "blue",
-      width=4
-    )
-  })
+  
+  #plotting using reactive dataset
+  output$epi_curve_total <- renderPlot({
+    p = ggplot(data=data_random(), aes(x, y)) + geom_point()
+    p
+    })
+  
+  
+  #randomly generates 3 numbers for state wide values
+  random_numbers <- reactive({
+    runif(3, min=0, max=100)
+    })
+  
+  
+  
+  
+# An idea to generate state wide boxes color coded on value using the randomly generated numbers above
+#Number of Cases
+output$state_case <- renderValueBox({valueBox(p(ceiling(random_numbers()[1])),"positive cases",
+                                     width=4,
+                                     color=ifelse(random_numbers()[1] > 50,"blue","yellow"),
+                                    )})
+
+#Hospitalization
+output$hospitalization <- renderValueBox({valueBox(p(ceiling(random_numbers()[2])),"hospitalizations",
+                                          width=4,
+                                          color=ifelse(random_numbers()[2] > 50,"blue","yellow")
+                                         )})
+  
+  
+#Current Restrictions  
+output$current_restrictions <- renderValueBox({valueBox(p(ceiling(random_numbers()[2])),"Current Restrictions",
+                                                 width=4,
+                                                 color=ifelse(random_numbers()[2] > 50,"blue","yellow")
+                                                )})
+  
+  
+  
   
   ## UNH Campus Situation -------------------------------------------------------
   
-  # camppus <- reactive({
-  #   input$campus
-  # })
-  # observeEvent(input$campus, { output$epi_curve_detail_title <- renderUI({ campus() })  })
-  
-  output$epi_curve_detail_title <- renderUI({
-    if(input$campus=="All"){
-      "Confirmed COVID-19 Cases in All Campuses"
-    }else{
-      glue("Confirmed COVID-19 Cases in {input$campus}")
-    }
-  })
-      
   # TO DO: replace this with function that builds epi curve for selected campus
   # using input$campus
   # Function should also take argument input$epi_curve_type to determine whether
   # the epi curve bars are split by role (student/employee) or on/off campus
-  output$epi_curve_detail <- renderPlot({
-    hist(rnorm(100), main=glue("Epi Curve - Detailed", "{input$campus}"), col="cornflowerblue")
-  })
+  output$epi_curve_detail <- renderPlot({hist(rnorm(100), main="Epi Curve - Detailed", col="cornflowerblue")})
   
   # TO DO: calculate total under isolation for given input$campus
   n_isol <- 7
@@ -52,4 +74,8 @@ function(input, output, session){
   # TO DO: make this a function to renderUI the entire box, not just value to be passed to UI
   output$n_isol_label <- renderText({glue("{n_isol}", " (", "{n_isol_sym}", ")")})
   
-}
+  ## Lab Testing --------------------------------------------------------
+  
+  }
+
+)
