90663f3d6118d538fa5734875e678a8dc036fcf9
sowmyavs1992 <60143575+sowmyavs1992@users.noreply.github.com>
2020-09-01 17:44:12 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 #function(request) 
 dashboardPage(
-
+  
   
   
   # HEADER --------------- 
@@ -38,13 +38,16 @@ dashboardPage(
                     .box{-webkit-box-shadow: none; 
                     -moz-box-shadow: none;
                     box-shadow: none;}
+                     #mytable {
+                     width: 80px;
+                     }
                      ")),
     tabItems(
       ## Home --------------------------------------------------------------------
       tabItem(
         tabName = "dashboard",
         div(style = "margin-top:-1em",  ##shaving off annoying blank space from the top
-        h4(strong("University of New Hampshire COVID-19 Dashboard"))#the html was interfering with render plot below. Need to figure out a workaround to display the data
+            h4(strong("University of New Hampshire COVID-19 Dashboard"))#the html was interfering with render plot below. Need to figure out a workaround to display the data
         ),
         column(width=6,
                fluidRow(
@@ -52,7 +55,7 @@ dashboardPage(
                      width=NULL,
                      solidHeader = TRUE,
                      title = "Confirmed COVID-19 Cases in UNH Community",
-                     plotOutput("epi_curve_total", height=270)
+                     plotOutput("epi_curve_total", height=248)
                  )# end box
                ),#end fluid row
                fluidRow(
@@ -78,43 +81,44 @@ dashboardPage(
         column(
           width=6,
           div(style = "font-size: 16px; vertical-align: middle",
-          box(
-            status="primary",
-            width=NULL,
-            solidHeader=TRUE,
-            title="College Operating Conditions",
-           
-            fluidRow(
-              box("",width=3,height=30, style="font-weight: bold"),
-              box("Durham",width=3,height=30,style="font-weight: bold; font-size: 13px"),
-              box("Manchester",width=3,height=30,style="font-weight: bold;font-size: 13px"),
-              box("Concord",width=3,height=30,style="font-weight: bold;font-size: 13px"),
-            ),
-            fluidRow(
-              box("Active Cases",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle"),
-              uiOutput("active_cases_durham"),
-              uiOutput("active_cases_manch"),
-              uiOutput("active_cases_concord")
-            ),
-            fluidRow(
-              box("Case Rate",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle"),
-              uiOutput("case_rates_durham"),
-              uiOutput("case_rates_manch"),
-              uiOutput("case_rates_concord")
-            ),
-            fluidRow(
-              box("% Isolation", br(), "Beds in Use",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle"),
-              uiOutput("pct_isol_durham"),
-              uiOutput("pct_isol_manch"),
-              uiOutput("pct_isol_concord")
-            ),
-            fluidRow(
-              box("% Quar.", br(), "Beds in Use",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle"),
-              uiOutput("pct_quar_durham"),
-              uiOutput("pct_quar_manch"),
-              uiOutput("pct_quar_concord")
-            )
-          ) # end box
+              box(
+                status="primary",
+                width=NULL,
+                solidHeader=TRUE,
+                title="College Operating Conditions",
+               div(style = "margin-top:0.04em;",
+                 fluidRow(
+                  box("",width=3,height=30, style="font-weight: bold",solidHeader=TRUE),
+                  box("Durham",width=3,height=30,style="font-weight: bold; font-size: 13px",solidHeader=TRUE),
+                  box("Manchester",width=3,height=30,style="font-weight: bold;font-size: 13px",solidHeader=TRUE),
+                  box("Concord",width=3,height=30,style="font-weight: bold;font-size: 13px",solidHeader=TRUE),
+                )
+                 ),
+                fluidRow(
+                  box("Active Cases",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle",solidHeader=TRUE),
+                  uiOutput("active_cases_durham"),
+                  uiOutput("active_cases_manch"),
+                  uiOutput("active_cases_concord")
+                ),
+                fluidRow(
+                  box("Case Rate",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle",solidHeader=TRUE),
+                  uiOutput("case_rates_durham"),
+                  uiOutput("case_rates_manch"),
+                  uiOutput("case_rates_concord")
+                ),
+                fluidRow(
+                  box("% Isolation", br(), "Beds in Use",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle",solidHeader=TRUE),
+                  uiOutput("pct_isol_durham"),
+                  uiOutput("pct_isol_manch"),
+                  uiOutput("pct_isol_concord")
+                ),
+                fluidRow(
+                  box("% Quar.", br(), "Beds in Use",width=3, height=80,style="font-weight: bold; font-size: 13px; vertical-align: middle",solidHeader=TRUE),
+                  uiOutput("pct_quar_durham"),
+                  uiOutput("pct_quar_manch"),
+                  uiOutput("pct_quar_concord")
+                )
+              ) # end box
           ) # end div
         )# End column 2
       ),# END tabItem
@@ -123,8 +127,8 @@ dashboardPage(
       tabItem(
         tabName="campus", 
         div(style = "margin-top:-1em", #the html was interfering with render plot below. Need to figure out a workaround to display the data
-        
-        h4(strong(textOutput("campus_text")))
+            
+            h4(strong(textOutput("campus_text")))
         ),
         
         ## START: lefthand column/box
@@ -133,92 +137,88 @@ dashboardPage(
                style = "background-color:#FFFFFF;", #Gave a uniform white background to the column. Remove if needed
                height=NULL,
                
-          # epi curve - tabset box with tabs for different views
-          fluidRow(
-            tabBox(title = h5("Epidemic Curve"),
-                   # The id lets us use input$tabset1 on the server to find the current tab
-                   id = "campus_epi_curve",
-                   width=NULL,
-                   height=240,
-                   tabPanel("On / Off campus", plotOutput("location_plot",height=200)),
-                   tabPanel("Student / Faculty", plotOutput("personnel_plot",height=200))
-            )
-            
-            ),# end fluidRow
-
-        # numeric indicators
-        # TO DO: figure out how to remove gray bars?
-          fluidRow(
-            div(style = "font-size: 12px; margin-top:3em",
-                fluidRow(
-                # TO DO: format these more nicely
-                  uiOutput("n_isol_label"),
-                  uiOutput("n_quar_label"),
-                  uiOutput("n_test_label")
-                ),
-                fluidRow(
-                  uiOutput("n_isol_value"),
-                  uiOutput("n_quar_value"),
-                  uiOutput("n_test_value")
-                )
-              )
-          ), # end fluidRow
-        
-        
-        # Dorm table 
-          fluidRow(
-            div(style = "font-size: 13px; margin-top:1em", ##reducing font size.
-                fluidRow(
-                dataTableOutput("mytable",width=5.5, height=50),
-                width=6,
-                height=70)
-              )
-          ) # end fluidRow
-          
-          ),# END lefthand column
+               # epi curve - tabset box with tabs for different views
+               fluidRow(
+                 tabBox(title = h5("Epidemic Curve"),
+                        # The id lets us use input$tabset1 on the server to find the current tab
+                        id = "campus_epi_curve",
+                        width=NULL,
+                        height=240,
+                        tabPanel("On / Off campus", plotOutput("location_plot",height=200)),
+                        tabPanel("Student / Faculty", plotOutput("personnel_plot",height=200))
+                 )
+                 
+               ),# end fluidRow
+               
+               # numeric indicators
+               # TO DO: figure out how to remove gray bars?
+               div(style = "font-size: 12px; margin-top:-1em;",##reducing font size to fit things in
+                   fluidRow(
+                     uiOutput("n_isol_label"),
+                     #),
+                     #fluidRow(
+                     uiOutput("n_quar"),
+                     #),
+                     #),
+                     #fluidRow(
+                     uiOutput("n_test"))
+               ), # end fluidRow
+               
+               
+               # Dorm table 
+               fluidRow(
+                 div(style = "font-size: 12px;", ##reducing font size.
+                     fluidRow(
+                       dataTableOutput("mytable", height=50),
+                       width=NULL,
+                       height=70)
+                 )
+               ) # end fluidRow
+               
+        ),# END lefthand column
         #not many changes made in campus tab after this. Testing statistics column still pending. Need more
         #details on this plot.
         
-      column(width=6,
-        ## START righthand column
-                  fluidRow(
-            # TO DO: add in testing figure; build in "tests not submitted" to ggplot object
-          ),
-          fluidRow(
-            box(
-              width=6,
-              p("Median days from sample collection to test result day")
-            ),
-            box(
-              width=2,
-              status="primary",
-              solidHeader = TRUE,
-              title = "UNH"
-              # TO DO: add in UNH sample dates
-            ),
-            box(
-              width=2,
-              status="primary",
-              solidHeader = TRUE,
-              title = "Quest"
-              # TO DO: add in Quest testing delays
-            ),
-            box(
-              width=2,
-              status="primary",
-              solidHeader = TRUE,
-              title = "CMD"
-              # TO DO: add in CMD testing delays
-            )
-            
-          )
+        column(width=6,
+               ## START righthand column
+               fluidRow(
+                 # TO DO: add in testing figure; build in "tests not submitted" to ggplot object
+               ),
+               fluidRow(
+                 box(
+                   width=6,
+                   p("Median days from sample collection to test result day")
+                 ),
+                 box(
+                   width=2,
+                   status="primary",
+                   solidHeader = TRUE,
+                   title = "UNH"
+                   # TO DO: add in UNH sample dates
+                 ),
+                 box(
+                   width=2,
+                   status="primary",
+                   solidHeader = TRUE,
+                   title = "Quest"
+                   # TO DO: add in Quest testing delays
+                 ),
+                 box(
+                   width=2,
+                   status="primary",
+                   solidHeader = TRUE,
+                   title = "CMD"
+                   # TO DO: add in CMD testing delays
+                 )
+                 
+               )
         ) # END righthand column
         
       ) # END campus page
-      )
-      
-    ) # END tabItems
-  ) # END dashboardBody
+    )
+    
+  ) # END tabItems
+) # END dashboardBody
 #) # END dashboardPage
 #} # END function
 
